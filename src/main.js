a98a4fdff3b496682ec1c168f3fc65a9c4cc61d9
Amin Forouzan <amin.pqr@gmail.com>
2025-09-05 21:08:05 +0330

    feat: add text analyze on user input

---
+++ src/main.js
@@ -0,0 +1,52 @@
+const elements = {
+  userText: document.getElementById("user-text"),
+  excludeSpaces: document.getElementById("exclude-spaces"),
+  chrLimit: document.getElementById("chr-limit"),
+  chrLimitCount: document.getElementById("chr-limit-count"),
+  readTime: document.getElementById("read-time"),
+  totalCharacters: document.getElementById("total-characters"),
+  wordCount: document.getElementById("word-count"),
+  sentenceCount: document.getElementById("sentence-count"),
+  hint: document.querySelector(".comp-textarea__hint"),
+};
+
+function showResult(totalCharacters, wordCount, sentenceCount, readTime) {
+  elements.totalCharacters.innerText = totalCharacters;
+  elements.wordCount.innerText = wordCount;
+  elements.sentenceCount.innerText = sentenceCount;
+  elements.readTime.innerText = `Approx. reading time: ${
+    readTime === 0 ? "<1" : readTime
+  } minute`;
+}
+
+function analyzeText(text) {
+  const textArr = text.trim().split("");
+  const excludeSpace = elements.excludeSpaces.checked;
+  const spaceCount = textArr.filter((chr) => chr === " ").length;
+  const totalCharacters = textArr.length - (excludeSpace ? spaceCount : 0);
+  const wordCount = spaceCount;
+  const sentenceCount = textArr.filter((chr) => chr === ".").length;
+  const readTime = Math.floor(wordCount / 238);
+
+  showResult(totalCharacters, wordCount, sentenceCount, readTime);
+}
+
+function checkLimit(text) {
+  const hasLimit = elements.chrLimit.checked;
+  const limitTreshhold = elements.chrLimitCount.valueAsNumber;
+  const limitReached = text.length > limitTreshhold;
+
+  if (hasLimit && limitReached) {
+    elements.userText.value = text.slice(0, limitTreshhold);
+    elements.hint.innerText = `Limit reached! Your text exceeds ${limitTreshhold} characters.`;
+  }
+
+  elements.hint.classList.toggle("hidden", !limitReached);
+  return elements.userText.value;
+}
+
+elements.userText.addEventListener("input", () => {
+  const userText = checkLimit(elements.userText.value);
+
+  analyzeText(userText);
+});
